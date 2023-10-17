d9a9d4508ccc7bb8a4e48a7b4583bec52fc9bf15
sadat <shadowak47@yahoo.com>
2023-10-17 18:01:46 +0100

    Removed Flatlists and performed noramal map() for ingredients and steps

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -6,6 +6,7 @@ import {
   Image,
   ScrollView,
   FlatList,
+  SectionList,
 } from "react-native";
 import { useRoute, useNavigation } from "@react-navigation/native";
 import { MEALS } from "../data/dummy-data";
@@ -17,9 +18,13 @@ const MealDetailsScreen = () => {
   const mealId = route.params.mealId;
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
-  const renderIngredientsItems = (itemData) => {
-    return <Text style={styles.textFont}>{itemData.item}</Text>;
-  };
+  // const renderIngredientsItems = (itemData) => {
+  //   return <Text style={styles.textFont}>{itemData.item}</Text>;
+  // };
+
+  // const renderSteps = (itemData) => {
+  //   return <Text style={styles.textFont}>{itemData.item}</Text>;
+  // };
 
   return (
     <ScrollView style={styles.screen}>
@@ -31,7 +36,7 @@ const MealDetailsScreen = () => {
       </View>
 
       <View style={styles.description}>
-        <Text style={styles.ingredients}>General</Text>
+        <Text style={styles.subHeadings}>General</Text>
         <Text style={styles.textFont}>
           <Text style={[styles.innerTags, styles.textFont]}>Duration:</Text>{" "}
           {selectedMeal.duration} minutes
@@ -48,12 +53,30 @@ const MealDetailsScreen = () => {
         </Text>
       </View>
       <View style={styles.description}>
-        <Text style={[styles.textFont, styles.ingredients]}>Ingredients</Text>
-        <FlatList
+        <Text style={[styles.textFont, styles.subHeadings]}>Ingredients</Text>
+        {/* <FlatList
           data={selectedMeal.ingredients}
           renderItem={renderIngredientsItems}
-          keyExtractor={item => item.id}
-        />
+          keyExtractor={(item) => item.id}
+        /> */}
+        {selectedMeal.ingredients.map((ingredient) => (
+          <Text style={styles.textFont} key={ingredient}>
+            {ingredient}
+          </Text>
+        ))}
+      </View>
+      <View style={styles.description}>
+        <Text style={styles.subHeadings}>Steps</Text>
+        {/* <FlatList
+          data={selectedMeal.steps}
+          renderItem={renderSteps}
+          keyExtractor={(item) => item.id}
+        /> */}
+        {selectedMeal.steps.map((step) => (
+          <Text style={styles.textFont} key={step}>
+            {step}
+          </Text>
+        ))}
       </View>
     </ScrollView>
   );
@@ -89,12 +112,12 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 22,
   },
-  ingredients: {
+  subHeadings: {
     fontSize: 20,
     textAlign: "center",
     color: "white",
     fontWeight: "500",
-    marginBottom: 8
+    marginBottom: 8,
   },
   description: {
     borderWidth: 1,
